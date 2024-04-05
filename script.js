7e05d126152097cb6e36655c7b52094f78297e87
popelkin-roman <mymaythe23@gmail.com>
2024-04-05 16:41:15 +0500

    Add coordinates

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ const drawCahrt = (data) => {
     const w = 1000;
     const h = 500;
     const padding = 50;
-    const minYear = d3.min(data, d => d.Year);
-    const maxYear = d3.max(data, d => d.Year);
-    const minTime = d3.min(data, d => d.Time);
-    const maxTime = d3.max(data, d => d.Time);
+    const minYear = new Date(0).setFullYear(d3.min(data, d => d.Year - 1));
+    const maxYear = new Date(0).setFullYear(d3.max(data, d => d.Year + 1));
+    const minTime = new Date("2000-01-01T00:"+ d3.min(data, d => d.Time));
+    const maxTime = new Date("2000-01-01T00:" + d3.max(data, d => d.Time));
 
-    const scaleYear = d3.scaleLinear()
+    const scaleYear = d3.scaleTime()
         .domain([minYear, maxYear])
         .range([padding, w - padding]);
     const scaleTime = d3.scaleTime()
-        .domain([new Date("2000-01-01T00:"+ minTime), new Date("2000-01-01T00:" + maxTime)])
+        .domain([minTime, maxTime])
         .range([padding, h - padding]);
         
     const svg = d3.select(".scatterplot")
@@ -29,12 +29,12 @@ const drawCahrt = (data) => {
         .data(data)
         .enter()
         .append("circle")
-        .attr("cx", (d) => scaleYear(d.Year) )
+        .attr("cx", (d) => scaleYear(new Date(0).setFullYear(d.Year)) )
         .attr("cy", (d) => scaleTime(new Date("2000-01-01T00:" + d.Time)))
         .attr("r", 5)
         .attr("class", "dot")
         .attr("data-xvalue", d => d.Year)
-        .attr("data-yvalue", d => d.Time)
+        .attr("data-yvalue", d => new Date("2000-01-01T00:" + d.Time))
         .on("mouseover", (e, d) => {
             d3.select("#tooltip")
                 .attr("data-year", d.Year)
@@ -46,31 +46,19 @@ const drawCahrt = (data) => {
             d3.select("#tooltip")
                 .style("visibility", "hidden");
         })
+    
+    const xAxis = d3.axisBottom(scaleYear);
+    const yAxis = d3.axisLeft(scaleTime)
+        .tickFormat(d3.timeFormat("%M:%S"));
+    const xAxisLine = svg.append("g")
+        .attr("id", "x-axis")
+        .attr("transform", `translate(${0}, ${h - padding})`)
+        .call(xAxis);
 
-    // const xAxisScale = d3.scaleTime()
-    //     .domain([firstDate, lastDate])
-    //     .range([padding, padding + (dataset.length-1)*width])
-    // const xAxis = d3.axisBottom(xAxisScale);
-
-    // const yAxisScale = d3.scaleLinear()
-    //     .domain([0,max])
-    //     .range([h-2*padding, 0]);
-    // const yAxis = d3.axisLeft(yAxisScale);
-
-    // const xAxisLine = svg.append("g")
-    //     .attr("id", "x-axis")
-    //     .attr("transform", `translate(${0}, ${h - padding})`)
-    //     .call(xAxis);
-
-    // let xAxisEl = xAxisLine.select(".domain");
-    // let newXAxisD = xAxisEl.attr("d").slice(0,-2).split("H")[0] + "H" +
-    //     (+xAxisEl.attr("d").slice(0,-2).split("H")[1] + width);
-    // xAxisEl.attr("d", newXAxisD);
-
-    // const yAxisLine = svg.append("g")
-    //     .attr("id", "y-axis")
-    //     .attr("transform", `translate(${padding}, ${padding})`)
-    //     .call(yAxis);
+    const yAxisLine = svg.append("g")
+        .attr("id", "y-axis")
+        .attr("transform", `translate(${padding}, ${0})`)
+        .call(yAxis);
 
     // let yAxisEl = yAxisLine.select(".domain");
     // let newYAxisD = yAxisEl.attr("d").slice(0,-2);
